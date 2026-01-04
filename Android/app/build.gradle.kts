8abe190bca390b7a16d2004e7edd46846e703225
Curtes Malteser <antonio_bastiao@hotmail.com>
2026-01-04 16:16:45 +0100

    feat: Enable context receivers
    Enables the `ContextParameters` language feature for Kotlin.

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
     kotlin {
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_21)
+            freeCompilerArgs.add("-XXLanguage:+ContextParameters")
         }
     }
     buildFeatures {
